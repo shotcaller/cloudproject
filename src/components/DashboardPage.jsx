6830605d81a831cf97c0cc754b745e40add7b016
shotcaller <ruturaj.ghodke77@gmail.com>
2024-11-22 17:01:26 -0400

    dashboard fourth chart

--- src/components/DashboardPage.jsx
+++ src/components/DashboardPage.jsx
@@ -1,33 +1,59 @@
 import { PieChart } from '@mui/x-charts/PieChart'
 import { BarChart } from '@mui/x-charts/BarChart'
+import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge'
 import { Box, Grid2, Paper, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 export const DashboardPage = () => {
 
-  let priorityTickets = {
+  const [userCount, setUserCount] = useState([0,0]) // customer , support
+  const [myTixCount, setMyTixCount] = useState(0) // my
+  const [allTixCount, setAllTixCount] = useState(0) //all tickets
+  const [priorityTix, setPriorityTix] = useState({
     low: [],
     medium: [],
     high: []
-  }
-  let customerTypes = {
-    customer: [],
-    customerSupport: []
-  }
-  const users = useSelector((state) => state.users, (data) => {
-    console.log(data.users)
-    data.users.forEach((user) => {
-      user.userRole==='Customer'?customerTypes.customer.push(user):customerTypes.customerSupport.push(user);
-    })
-  })
-  const tickets = useSelector((state) => state.tickets, (data) => {
-    const allTickets = [...data.open,...data.active,...data.closed];
-    allTickets.forEach(element => {
-      priorityTickets[`${element.priority.toLowerCase()}`].push(element);
-    });
-  
   });
+  const { userName } = useSelector((state) => state.user); 
+  
+
+  const users = useSelector((state) => state.users)
+  useEffect(() => {
+    let usCount = [0,0]
+    users.users.forEach((user) => {
+      user.userRole==='Customer'?usCount[0]+=1:usCount[1]+=1;
+  })
+  setUserCount(usCount);
+  }, [users])
+
+  const tickets  = useSelector((state) => state.tickets);
+  useEffect(() => {
+    const allTickets = [...tickets.open,...tickets.active,...tickets.closed];
+
+
+    let mytixCount = 0;
+    let priorityTickets = {
+      low: [],
+      medium: [],
+      high: []
+    }
+
+    allTickets.forEach(element => {
+      console.log("ticket", element, userName)
+      priorityTickets[`${element.priority.toLowerCase()}`].push(element);
+
+      if(element.assignedTo=== userName) {
+        mytixCount+=1;
+      }
+    
+    });
+    console.log(priorityTickets)
+    setMyTixCount(mytixCount);
+    setAllTixCount(allTickets.length)
+    setPriorityTix(priorityTickets);
+   
+  },[tickets])
 
   
   
@@ -59,24 +85,39 @@ export const DashboardPage = () => {
           <Grid2 size={{ sm: 12, md: 6 }} sx={{ justifyItems: 'center', background: '#F0F0F0'}}>
             <BarChart 
               xAxis={[{ scaleType: 'band', data: ['Customer', 'Customer Support']}]}
-              series={[{ data: [customerTypes.customer.length, customerTypes.customerSupport.length]}]}
+              series={[{ data: [userCount[0], userCount[1]]}]}
               width={400} height={300} />
               <Typography variant='h6'>Number of users by role</Typography>
 
           </Grid2>
-          <Grid2 size={{ sm: 12, md: 6 }} sx={{ justifyItems: 'center', background: '#F0F0F0'}}>
-            <BarChart 
-              xAxis={[{ scaleType: 'band', data: ['Customer', 'Customer Support']}]}
-              series={[{ data: [customerTypes.customer.length, customerTypes.customerSupport.length]}]}
-              width={400} height={300} />
+          <Grid2 size={{ sm: 12, md: 6 }} sx={{ justifyItems: 'center', alignContent: 'center',  background: '#F0F0F0'}}>
+          {/* <Gauge /> */}
+          <Gauge
+            value={myTixCount}
+            valueMax={allTixCount}
+            width={300}
+            height={200}
+            startAngle={-110}
+            endAngle={110}
+            sx={{
+              [`& .${gaugeClasses.valueText}`]: {
+                fontSize: 40,
+                transform: 'translate(0px, 0px)',
+              },
+            }}
+            text={
+              ({ value, valueMax }) => `${value} / ${valueMax}`
+            }
+          />
+            <Typography variant='h6'>Total tickets assigned to me</Typography>
           </Grid2>
           <Grid2 size={{ sm: 12, md: 6 }} sx={{ justifyItems: 'center', background: '#F0F0F0'}}>
           <PieChart series={[
               {
                 data: [
-                  {id: 0, value: priorityTickets.low.length, label: 'Low', color: 'green'},
-                  {id: 1, value: priorityTickets.medium.length, label: 'Medium', color: 'orange'},
-                  {id: 2, value: priorityTickets.high.length, label: 'High', color: 'red'},
+                  {id: 0, value: priorityTix.low.length, label: 'Low', color: 'green'},
+                  {id: 1, value: priorityTix.medium.length, label: 'Medium', color: 'orange'},
+                  {id: 2, value: priorityTix.high.length, label: 'High', color: 'red'},
   
                 ],
                 highlightScope: { fade: 'global', highlight: 'item' },
